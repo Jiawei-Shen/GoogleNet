fa487048e17d7f690016a25d40c8fe30e03d8a95
Jiawei-Shen <shenjiawei0116@gmail.com>
2025-07-09 13:52:59 -0500

    Update

--- mynet.py
+++ mynet.py
@@ -10,6 +10,33 @@ def same_padding_conv(in_channels, out_channels, kernel_size, stride=1, groups=1
     return nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=(ph, pw), groups=groups)
 
 
+class LayerNorm(nn.Module):
+    """ LayerNorm that supports two data formats: channels_last (default) or channels_first.
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
+    with shape (batch_size, channels, height, width).
+    """
+
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
 class GRN(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -81,7 +108,7 @@ class ConvNeXtBlock(nn.Module):
     def __init__(self, in_channels, out_channels, drop_path):
         super().__init__()
         self.dwconv = same_padding_conv(in_channels, in_channels, kernel_size=(7, 5), stride=1, groups=in_channels)
-        self.norm = nn.LayerNorm(in_channels)
+        self.norm = LayerNorm(in_channels, eps=1e-6)
         self.pwconv1 = nn.Linear(in_channels, 4 * in_channels)
         self.act = nn.GELU()
         self.grn = GRN(4 * in_channels)
@@ -117,7 +144,7 @@ class DownsampleLayer(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.downsample = nn.Sequential(
-            nn.LayerNorm(in_channels),
+            LayerNorm(in_channels, eps=1e-6, data_format="channels_first"),
             nn.Conv2d(in_channels, out_channels, kernel_size=2, stride=2),  # Normalize after Conv
             nn.GELU()          # Non-linearity
         )
@@ -130,7 +157,7 @@ class ConvNeXtCBAMClassifier(nn.Module):
         super().__init__()
         self.stem = nn.Sequential(
             nn.Conv2d(in_channels, 64, kernel_size=4, stride=4),
-            nn.LayerNorm(64),
+            LayerNorm(eps=1e-6, data_format="channels_first"),
             nn.GELU()
         )
 
